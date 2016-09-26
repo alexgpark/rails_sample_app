ee0a7da1b283da02f1d0c4b59a7dddbabbf44dd3
alexgpark <alexgpark@gmail.com>
2016-09-25 17:32:15 -0700

    added Reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  
   root "movies#index"
 
-  resources :movies
-  # 
+  resources :movies do
+  	resources :reviews
+  end
+
   # get "movies" => "movies#index"
   # get "movies/new" => "movies#new"
   # get "movies/:id" => "movies#show", as: "movie"
   # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
   # patch "movies/:id" => "movies#update"
+
 end
